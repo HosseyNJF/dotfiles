c82b7966f8ceb4cfa669b7faf854c51f2608d414
M. Hosseyn Najafi <hosseynjf@icloud.com>
2025-06-24 19:45:10 +0300

    feat(powershell): add aliases for powershell

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,15 @@
+<# Oh-My-Posh #>
+
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_lean.omp.json" | Invoke-Expression
+
+<# Aliases #>
+
+# Docker
+function dk { docker $args }
+function dkc { docker compose $args }
+
+# Kubernetes
+function k { kubectl $args }
+
+# Laravel
+function sail { ./vendor/bin/sail $args }
