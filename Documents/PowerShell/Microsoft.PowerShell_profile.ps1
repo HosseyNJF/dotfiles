8847301fbf0de290cb8dfe68fb31bd9a2e74f802
M. Hosseyn Najafi <hosseynjf@icloud.com>
2025-07-05 18:01:23 +0300

    feat(omp): add instant prompt for oh my posh

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,17 @@
 <# Oh-My-Posh #>
 
-oh-my-posh init pwsh --config "$env:USERPROFILE/.powerlevel10k.omp.json" | Invoke-Expression
+function prompt {
+  if (Test-Path variable:global:ompjob) {
+    Receive-Job -Wait -AutoRemoveJob -Job $global:ompjob | Invoke-Expression;
+    Remove-Variable ompjob -Scope Global;
+    return prompt;
+  }
+  $global:ompjob = Start-Job {oh-my-posh init pwsh --config "$env:USERPROFILE/.powerlevel10k.omp.json"};
+  write-host -ForegroundColor Blue "Loading `$profile in the background..."
+  Write-Host -ForegroundColor Blue -NoNewline "  $($executionContext.SessionState.Path.CurrentLocation) ".replace($HOME, '~');
+  Write-Host -ForegroundColor Green -NoNewline "❯"
+  return " ";
+}
 
 <# Aliases #>
 
@@ -10,6 +21,13 @@ function dkc { docker compose $args }
 
 # Kubernetes
 function k { kubectl $args }
+
+<# Tab and Up/Down arrow completion like zsh #>
+
+Import-Module PSReadLine;
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 # Laravel
 function sail { ./vendor/bin/sail $args }
