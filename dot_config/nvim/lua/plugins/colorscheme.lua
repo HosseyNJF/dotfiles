faca34b298f18c944686eda189c50851243fe129
M. Hosseyn Najafi <hosseyn.najafi@teampicnic.com>
2026-04-30 00:18:59 +0200

    fix(nvim): fix colorscheme issues and cleanup file

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,3 @@
--- return {
---   "maxmx03/solarized.nvim",
---   lazy = false,
---   priority = 1000,
---   ---@type solarized.config
---   opts = {},
---   config = function(_, opts)
---     vim.o.termguicolors = true
---     vim.o.background = "light"
---     require("solarized").setup(opts)
---     vim.cmd.colorscheme("solarized")
---   end,
--- }
--- return {
---   "calind/selenized.nvim",
---   config = function(_, opts)
---     vim.o.termguicolors = true
---     vim.o.background = "light"
---     -- require("solarized").setup(opts)
---     vim.cmd.colorscheme("selenized")
---   end,
--- }
-
 return {
   {
     "catppuccin/nvim",
@@ -34,7 +11,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "catppuccin-frappe",
     },
   },
 }
